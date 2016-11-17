2cb7f967f336aef48ff04cf017347b1908080d98
Nathan Frend <theniffin@gmail.com>
2016-11-17 15:49:50 +0000

    Add: makefile and simple server that listens to port found in config.yml

--- Server.java
+++ Server.java
@@ -1,98 +1,79 @@
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.*;
 
 public class Server
 {
-    ServerSocket serverSocket;
-    Socket client;
-    OutputStream os;
-    PrintWriter out;
-    InputStream is;
-    BufferedReader in;
-    String phrase;
+    private ServerSocket serverSocket;
+    private String serverAddress;
+    private int serverPort;
+    private BufferedReader clientIn;
 
     public Server() throws IOException
     {
         try {
             //prepare file reader and buffer
-            FileReader fileReader = new FileReader("server.config");
+            FileReader fileReader = new FileReader("config.yml");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String readLine;
 
-            //three lines of addresses: NSW, SSW and server
-            nsw = new NorthSideWrap(Integer.parseInt(bufferedReader.readLine()));
-            ssw = new SouthSideWrap(Integer.parseInt(bufferedReader.readLine()));
-            serverSocket = new ServerSocket(Integer.parseInt(bufferedReader.readLine()));
+            while ((readLine = bufferedReader.readLine()) != null) {
+                String[] parsedLine = readLine.split("[: ]");
+                switch (parsedLine[0]) {
+                    case "hostname":
+                        this.serverAddress = parsedLine[2];
+                        break;
+                    case "port":
+                        this.serverPort = Integer.parseInt(parsedLine[2]);
+                        break;
+                }
+            }
 
             //close the file
             bufferedReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Couldn't find the client config file");
-        } catch (IOException e) {
+        } catch (Exception e) {
+            System.out.println(e);
             System.out.println("File reading error");
             System.out.println("Make sure addresses are in correct format");
         }
-        phrase = "Hello world!";
+
+        System.out.println("Initialising server on " + this.serverAddress + ":" + this.serverPort);
+        this.serverSocket = new ServerSocket(this.serverPort);
     }
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {
         Server server = new Server();
-        server.connectLogger();
         server.connectClient();
         server.read();
-        server.write();
         server.close();
     }
 
-    private void connectLogger() throws IOException, SocketException {
-        //need to connect the logger via NSW and SSW
-        nsw.connect();
-        ssw.connect();
-    }
-
-    private void connectClient() throws IOException, SocketException {
+    private void connectClient() throws IOException, SocketException
+    {
         System.out.println("Waiting on client");
-        client = serverPort.accept();
+        Socket client = serverSocket.accept();
         System.out.println("Client connected");
 
         //Setup streams
-        os = client.getOutputStream();
-        out = new PrintWriter(os, true);
-        is = client.getInputStream();
-        in = new BufferedReader(new InputStreamReader(is));
-
+        clientIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
     }
 
-    private void read() throws IOException, SocketException {
-        String words = in.readLine();
+    private void read() throws IOException, SocketException
+    {
+        String words = clientIn.readLine();
         System.out.println("Have read in from the client: ");
         System.out.println(words);
     }
 
-    private void write() throws IOException, SocketException {
-        System.out.println("Directing to client: " + phrase);
-        out.println(phrase);
-    }
-
-    private void close() throws IOException, SocketException {
+    private void close() throws IOException, SocketException
+    {
         System.out.println("Closing server");
-        out.close();
-        in.close();
-        os.close();
-        is.close();
-        client.close();
-        nsw.close();
-        ssw.close();
-        serverPort.close();
+        serverSocket.close();
+        clientIn.close();
     }
 }
